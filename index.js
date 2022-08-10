43bbc89a3bb140a1abb3e37c50d2b21f39efa05b
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-08-10 14:53:50 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+    
+}
+function subtract() {
+    
+}
+function multiply() {
+    
+}
+function divide() {
+    
+}
+function add(a,b) {
+    return a + b;
+}
+add(4,5);
+function subtract(a,b) {
+    return a - b;
+}
+subtract(4,5);
+function multiply(a,b) {
+    return a * b;
+}
+multiply(4,5);
+function divide(a,b) {
+    return a / b;
+}
+divide(4,5);
+
+function increment(n) {
+    return n+1;
+}
+increment(5);
+function decrement(n) {
+    return n-1;
+}
+decrement(5);
+
+function makeInt(n) {
+    n = parseInt("a");
+}
+function makeInt(n) {
+    return n;
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
